d16f42685cba972adf65a8f68434dc837af06aab
Paolo Lella <xzekro91@gmail.com>
2021-07-22 10:04:28 +0200

    Added exclusive vintage pokemons

--- Xenoverse/Data/Scripts/235_Retromon.rb
+++ Xenoverse/Data/Scripts/235_Retromon.rb
@@ -99,6 +99,33 @@ RETROMON = {
 				:QUAGSIRE=> :QUAGSIREVINTAGE
 
 }
+
+EXCLUSIVERETROMON = {
+	#MEOWSY
+	38 => [1396],
+	#SUNMOLA
+	351 => [1431],
+	#BOMSEAKER
+	109 => [1434],
+	119 => [1434],
+	120 => [1434],
+	121 => [1434],
+	123 => [1434],
+	124 => [1434],
+	126 => [1434],
+	129 => [1434],
+	252 => [1434],
+	253 => [1434],
+	282 => [1434],
+	283 => [1434],
+	284 => [1434],
+	285 => [1434],
+	455 => [1434],
+	#TIGRETTE
+	181 => [1435],
+	#WOLFMAN
+	281 => [1437]
+}
 for k in RETROMON.keys
 	RETROMON[getID(PBSpecies,k)] = getID(PBSpecies,RETROMON[k])
 end
@@ -127,16 +154,30 @@ def pbEncounter(enctype)
     if $Trainer.retrochain[encounter[0]]==nil
       $Trainer.retrochain[encounter[0]]=0
     end
-		ch = 1 + $Trainer.retrochain[encounter[0]]/63
-		echoln encounter[0]
-		echoln RETROMON[getConst(PBSpecies,encounter[0])]
-		if RETROMON[encounter[0]] != nil && (($game_switches[RETROMONSWITCH] && rand(4000)<ch) || $allRetro)
-			echoln "triggered retro"
-			pbWildBattle(RETROMON[encounter[0]],encounter[1])
+	ch = 1 + $Trainer.retrochain[encounter[0]]/63
+	echoln encounter[0]
+	echoln RETROMON[getConst(PBSpecies,encounter[0])]
+	if (($game_switches[RETROMONSWITCH] && rand(4000)<ch) || $allRetro)
+		echoln "triggered retro"
+		if RETROMON[encounter[0]] != nil
+			#if i'm in a special area, i want a 50/50 chance of encountering a special retromon if i would encounter a retromon
+			if (EXCLUSIVERETROMON.has_key?($game_map.map_id))
+				if (rand(100)>50)
+					pbWildBattle(RETROMON[encounter[0]],encounter[1])
+				else
+					pbWildBattle(EXCLUSIVERETROMON[$game_map.map_id][0],encounter[1])
+				end
+			else
+				pbWildBattle(RETROMON[encounter[0]],encounter[1])
+			end
+		elsif !RETROMON.has_key?(encounter[0]) && EXCLUSIVERETROMON.has_key?($game_map.map_id)
+			#if i wouldn't find a retromon, i still check if i could encounter any exclusive
+			pbWildBattle(EXCLUSIVERETROMON[$game_map.map_id][0],encounter[1])
 		else
 			pbWildBattle(encounter[0],encounter[1])
 		end
-	  $PokemonTemp.encounterType=-1
+	end
+	$PokemonTemp.encounterType=-1
     $fishing = false if enctype == EncounterTypes::OldRod || enctype ==EncounterTypes::GoodRod || enctype == EncounterTypes::SuperRod
     return true
   end
@@ -174,11 +215,25 @@ def pbBattleOnStepTaken
             ch = 1 + $Trainer.retrochain[encounter[0]]/63
 			echoln "encounter #{encounter[0]}"
 			echoln "retro encounter #{RETROMON[encounter[0]]}"
-			if RETROMON[encounter[0]] != nil && (($game_switches[RETROMONSWITCH] && rand(4000)<ch) || $allRetro)
+			if (($game_switches[RETROMONSWITCH] && rand(4000)<ch) || $allRetro)
 				echoln "triggered retro"
-				pbWildBattle(RETROMON[encounter[0]],encounter[1])
-			else
-				pbWildBattle(encounter[0],encounter[1])
+				if RETROMON[encounter[0]] != nil
+					#if i'm in a special area, i want a 50/50 chance of encountering a special retromon if i would encounter a retromon
+					if (EXCLUSIVERETROMON.has_key?($game_map.map_id))
+						if (rand(100)>50)
+							pbWildBattle(RETROMON[encounter[0]],encounter[1])
+						else
+							pbWildBattle(EXCLUSIVERETROMON[$game_map.map_id][0],encounter[1])
+						end
+					else
+						pbWildBattle(RETROMON[encounter[0]],encounter[1])
+					end
+				elsif !RETROMON.has_key?(encounter[0]) && EXCLUSIVERETROMON.has_key?($game_map.map_id)
+					#if i wouldn't find a retromon, i still check if i could encounter any exclusive
+					pbWildBattle(EXCLUSIVERETROMON[$game_map.map_id][0],encounter[1])
+				else
+					pbWildBattle(encounter[0],encounter[1])
+				end
 			end
           end
         end
