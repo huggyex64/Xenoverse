4f835027cd8233a0f2fb94713bb1a41c0f2cec15
Paolo Lella <xzekro91@gmail.com>
2021-09-18 10:09:47 +0200

    Update 001_Settings.rb

--- Xenoverse/Data/Scripts/001_Settings.rb
+++ Xenoverse/Data/Scripts/001_Settings.rb
@@ -454,7 +454,7 @@ class Version < Array
    end
 end
 
-GAME_VERSION = Version.new("1.2.7")
+GAME_VERSION = Version.new("1.2.9")
 
 def pbTestVersions
    echoln Version.new('1.2') < Version.new('1.2.1')
